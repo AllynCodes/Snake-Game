62246df5b82113873aead638aeb60d5d1d0a9c33
Allyn McKenna Patterson <allyncodes@gmail.com>
2022-12-28 16:46:03 +0000

    Created snake

--- snake.js
+++ snake.js
@@ -2,10 +2,13 @@
 //board
 var blockSize = 25;
 var rows = 20;
-var cols = 20;
+var cols = 40;
 var board;
-var context; //Daring object
+var context; //Drawing object
 
+//Snake head
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -19,4 +22,7 @@ window.onload = function() {
 function update(){
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
+
+    context.fillStyle="lime";
+    context.fillRect(snakeX, snakeY, blockSize, blockSize);
 }
